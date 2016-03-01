a0cc6f1acd0f02c33698cf1a5c290d200f55ff2d
ssergienko <sergei_sergienko@epam.com>
2016-03-01 11:38:56 +0300

    Fix left menu, right menu turned to plus

--- app/components/menu/menu-directive.js
+++ app/components/menu/menu-directive.js
@@ -7,9 +7,7 @@ angular.module('w2dmApp.menu', [])
             templateUrl: 'components/menu/menu.html',
             controller: ['$scope', function ($scope) {
                 var menuLeft = document.getElementById('cbp-spmenu-s1'),
-                    menuRight = document.getElementById('cbp-spmenu-s2'),
                     showLeft = document.getElementById('left-menu-button'),
-                    showRight = document.getElementById('right-menu-button'),
                     body = document.body;
 
                 showLeft.onclick = function() {
@@ -24,28 +22,15 @@ angular.module('w2dmApp.menu', [])
                         disableOther('body');
                         classie.remove( menuLeft, 'cbp-spmenu-open' );
                     }
-                    if (e.target.id != showRight.id && angular.element(e.target).parent()[0].id != showRight.id &&
-                        e.target.id != menuRight.id && angular.element(e.target).parent()[0].id != menuRight.id)
-                    {
-                        disableOther('body');
-                        classie.remove( menuRight, 'cbp-spmenu-open' );
-                    }
-                };
-                showRight.onclick = function() {
-                    classie.toggle( this, 'active' );
-                    classie.toggle( menuRight, 'cbp-spmenu-open' );
-                    disableOther( 'showRight' );
                 };
 
                 $scope.closeMenu = function($mdCloseMenu, ev) {
                     disableOther('body');
-                    classie.remove( menuRight, 'cbp-spmenu-open' );
                     classie.remove( menuLeft, 'cbp-spmenu-open' );
                 }
 
                 function disableOther( button ) {
                     if( button !== 'showLeft' ) { classie.toggle( showLeft, 'disabled' ); }
-                    if( button !== 'showRight' ) { classie.toggle( showRight, 'disabled' ); }
                 }
 
             }]
