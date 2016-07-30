655b8417a3469167c7a01714fa288c1261ab2e20
Sserg Mac <sserg@mack.local>
2016-07-30 22:55:08 +0300

    Api changed to api.way2day.ru

--- src/services/placesService.js
+++ src/services/placesService.js
@@ -2,10 +2,10 @@
  * Created by Sergei_Sergienko on 3/9/2016.
  */
 angular.module('w2dmApp')
-    .factory('placesService',['$resource', function ($resource) {
-        return $resource('http://devel.way2day.ru/api/places/getbycoords?lat=59.939095&lng=30.315868&_=1457518554341', {},
-            {
-                'get': { method:'GET', isArray: true }
-            }
-        );
+  .factory('placesService',['$resource', function ($resource) {
+    return $resource('http://api.way2day.ru/api/places', {},
+      {
+        'get': { method:'GET', isArray: true }
+      }
+    );
 }]);
